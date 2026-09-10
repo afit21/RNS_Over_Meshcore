86a4f8149059b2d51bffc0a7f548793bc5e63dbb
Afi <46953457+afit21@users.noreply.github.com>
2026-09-10 10:51:43 +0930

    Change path discovery target from target_key to contact
    Updated path discovery command to use 'contact' instead of 'target_key'.

--- Interface/MeshCore_Dynamic_Interface.py
+++ Interface/MeshCore_Dynamic_Interface.py
@@ -2,6 +2,27 @@
 MeshCore_Dynamic_Interface.py
 Reticulum (RNS) interface over a MeshCore LoRa mesh network.
 
+TODO:
+    Review issues:
+    - Retransmission queue (transmits don't pass all: mode, target, frag_str, queued_at, pkt_id)
+    - Race condition in route learning (In _process_tunnel_text())
+    - Malformed packet crash (address by checking length before decode)
+    
+    Proposed changes:
+    - Log fail for retransmit fallback (After except queue.Full:)
+    - replace time.time() with time.monotonic()
+
+    Core optimisations:
+    - [Big] Split direct and channel queues
+    - [Big] Adaptive fragment sizing (get node name, and firmware limit, size the payload accordingly). Adapt fragment size if direct, channel, and based on node names. Save 3 bytes at end for repeater info
+    - Make retransmissions fragment-aware (retransmit currently retransmits every fragment, regardless of if some arrived)
+    - Replace periodic contact refresh with on direct route failure and/or path length unknown
+
+    Byte Optimisations:
+    - Shorten 'RNS:' to 'R:'
+    - Swap to 2 byte packet id instead of 4 byte.
+    - Reduce rns advert message sizes
+
 Implements a hybrid channel-broadcast / unicast-direct routing strategy with
 demand-driven peer discovery and edge-node capability advertisement.  No static
 remote-node configuration is required.
@@ -1497,7 +1518,7 @@ class MeshCore_Dynamic_Interface(Interface):
                                 if self._loop is not None:
                                     RNS.log(f"requesting path discovery for peer key {target_key}", RNS.LOG_INFO)
                                     asyncio.run_coroutine_threadsafe(
-                                        self._mc.commands.send_path_discovery_sync(target_key), 
+                                        self._mc.commands.send_path_discovery_sync(contact), 
                                         self._loop
                                     )
                                     
